fc5c0dd697255a490f97c058c896080458a93152
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-02-18 11:41:25 -0500

    filled out file for credntials

--- src/main/java/com/example/javafxadvanced/db/Credentials.java
+++ src/main/java/com/example/javafxadvanced/db/Credentials.java
@@ -1,4 +1,17 @@
 package com.example.javafxadvanced.db;
 
 public class Credentials {
+    private static final String DB_URL = "";
+    private static final String DB_USER = "";
+    private static final String DB_PASS = "";
+
+    public static String getDbUrl(){
+        return DB_URL;
+    }
+    public static String getDbUser(){
+        return DB_USER;
+    }
+    public static String getDbPass(){
+        return DB_PASS;
+    }
 }
