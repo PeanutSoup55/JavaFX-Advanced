166b71c5f1e7f3124e1d266cef4a9aafedd23ec3
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-02-21 13:55:20 -0500

    wrote saleItem code repurposed form last project

--- src/main/java/com/example/javafxadvanced/Objects/SaleItem.java
+++ src/main/java/com/example/javafxadvanced/Objects/SaleItem.java
@@ -1,4 +1,45 @@
 package com.example.javafxadvanced.Objects;
 
 public class SaleItem {
+    private int id;
+    private int saleId;
+    private int productId;
+    private String productName;
+    private int quantity;
+    private double unitCOG;
+    private double unitPrice;
+    private double totalPrice;
+
+    public SaleItem(int productId, String productName, int quantity, double unitPrice, double unitCOG) {
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.unitCOG = unitCOG;
+        this.totalPrice = quantity * unitPrice;
+    }
+
+    public SaleItem(int id, int saleId, int productId, String productName, int quantity, double unitPrice, double totalPrice) {
+        this.id = id;
+        this.saleId = saleId;
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getId() { return id; }
+    public int getSaleId() { return saleId; }
+    public int getProductId() { return productId; }
+    public String getProductName() { return productName; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        this.totalPrice = quantity * unitPrice;
+    }
+    public double getUnitPrice() { return unitPrice; }
+    public double getUnitCOG() { return unitCOG; }
+    public double getTotalCOG() { return quantity * unitCOG; }
+    public double getTotalPrice() { return totalPrice; }
 }
