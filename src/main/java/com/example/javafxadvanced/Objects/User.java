4300fada7f14ff064eb15a184981845bead88e9b
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-02-21 14:30:56 -0500

    wrote User object code

--- src/main/java/com/example/javafxadvanced/Objects/User.java
+++ src/main/java/com/example/javafxadvanced/Objects/User.java
@@ -1,4 +1,99 @@
 package com.example.javafxadvanced.Objects;
 
+import java.sql.Timestamp;
+
 public class User {
+    private int id;
+    private int company_id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private double pay;
+    private String role;
+    private Timestamp deletedAt;
+
+    public User(int id, int company_id, String username, String password, String email, String phone, double pay, String role, Timestamp deletedAt) {
+        this.id = id;
+        this.company_id = company_id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.pay = pay;
+        this.role = role;
+        this.deletedAt = deletedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCompany_id() {
+        return company_id;
+    }
+
+    public void setCompany_id(int company_id) {
+        this.company_id = company_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Timestamp getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Timestamp deletedAt) {
+        this.deletedAt = deletedAt;
+    }
 }
