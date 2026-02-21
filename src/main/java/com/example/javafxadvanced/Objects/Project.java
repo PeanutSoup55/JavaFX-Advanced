52f3db958fe69f30291b70ba660ec9c5de4d4450
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-02-21 14:20:48 -0500

    wrote project object code

--- src/main/java/com/example/javafxadvanced/Objects/Project.java
+++ src/main/java/com/example/javafxadvanced/Objects/Project.java
@@ -1,4 +1,67 @@
 package com.example.javafxadvanced.Objects;
 
 public class Project {
+    private int id;
+    private int companyId;
+    private String name;
+    private String description;
+    private String status;
+    private String createdAt;
+
+    public Project(int id, int companyId, String name, String description, String status, String createdAt) {
+        this.id = id;
+        this.companyId = companyId;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
 }
