c94868a5ab30d02b233dc75412c262b93e90fe5e
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-02-21 14:55:22 -0500

    wrote Log object code

--- src/main/java/com/example/javafxadvanced/Objects/Log.java
+++ src/main/java/com/example/javafxadvanced/Objects/Log.java
@@ -1,4 +1,89 @@
 package com.example.javafxadvanced.Objects;
 
+import java.sql.Timestamp;
+
 public class Log {
+    private int id;
+    private int companyId;
+    private int userId;
+    private String action;
+    private String entityType;
+    private int entityId;
+    private String description;
+    private Timestamp createdAt;
+
+    public Log(int id, int companyId, int userId, String action, String entityType, int entityId, String description, Timestamp createdAt) {
+        this.id = id;
+        this.companyId = companyId;
+        this.userId = userId;
+        this.action = action;
+        this.entityType = entityType;
+        this.entityId = entityId;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getEntityType() {
+        return entityType;
+    }
+
+    public void setEntityType(String entityType) {
+        this.entityType = entityType;
+    }
+
+    public int getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
