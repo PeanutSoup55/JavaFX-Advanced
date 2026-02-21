6ce3f572ea4f72a3339c5fb010249b7308dd4caa
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-02-21 14:43:54 -0500

    wrote Task object code

--- src/main/java/com/example/javafxadvanced/Objects/Task.java
+++ src/main/java/com/example/javafxadvanced/Objects/Task.java
@@ -1,4 +1,100 @@
 package com.example.javafxadvanced.Objects;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class Task {
+    private int id;
+    private int companyId;
+    private int projectId;
+    private int assignedTo;
+    private String title;
+    private String description;
+    private String status;
+    private Date dueDate;
+    private Timestamp createdAt;
+
+    public Task(int id, int companyId, int projectId, int assignedTo, String title, String description, String status, Date dueDate, Timestamp createdAt) {
+        this.id = id;
+        this.companyId = companyId;
+        this.projectId = projectId;
+        this.assignedTo = assignedTo;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public int getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(int assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
