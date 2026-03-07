8eb00ffbd318a3f4ef03950ba803f0c692c24fee
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-03-07 14:37:17 -0500

    created feedback class

--- src/main/java/com/example/javafxadvanced/Objects/Feedback.java
+++ src/main/java/com/example/javafxadvanced/Objects/Feedback.java
@@ -1,4 +1,26 @@
 package com.example.javafxadvanced.Objects;
 
+import java.sql.Timestamp;
+
 public class Feedback {
+    private int id;
+    private int company_id;
+    private int customer_id;
+    private int user_id;
+    private String type;
+    private String message;
+    private int rating;
+    private Timestamp created_at;
+
+    public Feedback(int id, int company_id, int customer_id, int user_id, String type, String message, int rating) {
+        this.id = id;
+        this.company_id = company_id;
+        this.customer_id = customer_id;
+        this.user_id = user_id;
+        this.type = type;
+        this.message = message;
+        this.rating = rating;
+    }
+
+
 }
